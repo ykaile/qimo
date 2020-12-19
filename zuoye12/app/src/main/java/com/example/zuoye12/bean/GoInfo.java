8084ed6f18f6008b6e613ce2dc53388ae7c48ef8
ykaile <1335760681@qq.com>
2020-12-19 12:11:41 +0800

    期末作业

---
+++ zuoye12/app/src/main/java/com/example/zuoye12/bean/GoInfo.java
@@ -0,0 +1,61 @@
+package com.example.zuoye12.bean;
+
+import com.example.zuoye12.R;
+
+import java.util.ArrayList;
+
+public class GoInfo {
+    public int pic_id;
+    public String title;
+    public String desc;
+    public boolean bPressed;
+    public int id;
+    private static int seq = 0;
+
+    public GoInfo(int pic_id, String title, String desc) {
+        this.pic_id = pic_id;
+        this.title = title;
+        this.desc = desc;
+        this.bPressed = false;
+        this.id = this.seq;
+        this.seq++;
+    }
+    private static int[] gridImageArray = {R.drawable.pic_01, R.drawable.pic_02, R.drawable.pic_03
+            , R.drawable.pic_04, R.drawable.pic_05, R.drawable.pic_06, R.drawable.pic_07
+            , R.drawable.pic_08, R.drawable.pic_09, R.drawable.pic_10};
+    private static String[] gridTitleArray = {"商场", "超市", "百货", "便利店",
+            "地摊", "食杂店", "饭店", "餐厅", "会所", "菜市场"};
+
+    public static ArrayList<GoInfo> getDefaultGrid() {
+        ArrayList<GoInfo> gridArray = new ArrayList<GoInfo>();
+        for (int i = 0; i < gridImageArray.length; i++) {
+            gridArray.add(new GoInfo(gridImageArray[i], gridTitleArray[i], null));
+        }
+        return gridArray;
+    }
+
+
+
+    private static int[] stagImageArray = {R.drawable.skirt01, R.drawable.skirt02, R.drawable.skirt03
+            , R.drawable.skirt04, R.drawable.skirt05, R.drawable.skirt06, R.drawable.skirt07
+            , R.drawable.skirt08, R.drawable.skirt09, R.drawable.skirt10, R.drawable.skirt11
+            , R.drawable.skirt12, R.drawable.skirt13, R.drawable.skirt14, R.drawable.skirt15
+            , R.drawable.skirt16, R.drawable.skirt17, R.drawable.skirt18, R.drawable.skirt19
+            , R.drawable.skirt20, R.drawable.skirt21, R.drawable.skirt22, R.drawable.skirt23};
+    private static String[] stagTitleArray = {"促销价", "惊爆价", "跳楼价", "白菜价", "清仓价", "割肉价",
+            "实惠价", "一口价", "满意价", "打折价", "腰斩价", "无人问津", "算了吧", "大声点",
+            "嘘嘘", "嗯嗯", "呼呼", "呵呵", "哈哈", "嘿嘿", "嘻嘻", "嗷嗷", "喔喔"};
+
+    public static ArrayList<GoInfo> getDefaultStag() {
+        ArrayList<GoInfo> stagArray = new ArrayList<GoInfo>();
+        for (int i = 0; i < stagImageArray.length; i++) {
+            stagArray.add(new GoInfo(stagImageArray[i], stagTitleArray[i], null));
+        }
+        return stagArray;
+    }
+
+
+
+
+
+}
